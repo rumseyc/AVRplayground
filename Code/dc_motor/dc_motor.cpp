096feb98cdc1896868579cd364baed66bb85f4b5
Carl Rumsey <rumseyc@mail.gvsu.edu>
2016-12-16 00:18:26 -0500

    Add in peter fleury uart driver and move i2c to driver dir

--- Code/dc_motor/dc_motor.cpp
+++ Code/dc_motor/dc_motor.cpp
@@ -13,6 +13,7 @@ int main() {
   uint8_t motor_speed = 0xFF;
 
   DDRB |= _BV(PB1); //Set PB1 to output
+  DDRC |= _BV(PC4); //Set PC4 to output
   //Clear OCnA register on compare match
   //WGM10 & WGM12 Set to 8 bit fast PWM mode
   //Use 1:1 Prescaler
@@ -21,8 +22,12 @@ int main() {
   TCCR1A |= _BV(COM1A1); //Turn on clock after its configured
 
   while(1) {
-    OCR1A = motor_speed;
-    _delay_ms(25);
+    OCR1A = 0xFF;
+    PORTC |= _BV(PC4);//Turn on led
+    _delay_ms(2000);
+    OCR1A = 0x00;
+    PORTC &= ~_BV(PC4);
+    _delay_ms(2000);
   }
 
   return 0;
